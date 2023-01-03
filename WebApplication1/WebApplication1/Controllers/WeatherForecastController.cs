9f235ca64562136676c4b859ad0969370f5405e0
SkylerOB <skyler017@revature.net>
2023-01-03 08:12:28 -0800

    slash to the end

--- WebApplication1/WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/WebApplication1/Controllers/WeatherForecastController.cs
@@ -4,7 +4,7 @@ namespace WebApplication1.Controllers
 {
     [ApiController]
     //[Route("[controller]")]
-    [Route("/weather")]
+    [Route("weather/")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
@@ -32,7 +32,7 @@ namespace WebApplication1.Controllers
         }
 
         
-        [HttpGet("/everything")]
+        [HttpGet("everything")]
         public IEnumerable<WeatherForecast> GetEverything()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
